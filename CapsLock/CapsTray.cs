9290a58fff98b32b603d267492f5d4895dd94629
Eric Chubb <chubbe@tcd.ie>
2015-08-07 15:31:27 +0100

    Initial commit
    Capslock beta

---
+++ CapsLock/CapsTray.cs
@@ -0,0 +1,123 @@
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2015 ericcdub
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+
+using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace CapsTray
+{
+    class CapsTray
+    {
+
+        //declare unmanaged functions used to intercept caps lock keypresses
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr SetWindowsHookEx(int idHook,KeyboardDelegate kbDelegate, IntPtr hMod, uint dwThreadId);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool UnhookWindowsHookEx(IntPtr hhk);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr CallNextHookEx(IntPtr hhk, int nCode, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        private static extern IntPtr GetModuleHandle(string lpModuleName);
+
+        // our delegate which will refer to our callback method later on
+        private delegate IntPtr KeyboardDelegate(int nCode, IntPtr wParam, IntPtr lParam);
+
+        private const int WH_KEYBOARD_LL = 13;
+        private const int WM_KEYDOWN = 0x0100;
+        private static IntPtr hookId = IntPtr.Zero;
+        private static CapsTrayIcon icon;
+
+        /// <summary>
+        /// Application entry point
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            // Show system tray icon.
+            using (icon = new CapsTrayIcon())
+            {
+                icon.Display();
+
+                // Set hook to intercept caps lock press
+                hookId = setKbHook(callback);
+
+                // begin message loop
+                Application.Run();
+                
+                //unset hook once app exits
+                UnhookWindowsHookEx(hookId);
+            }
+
+        }
+
+        /// <summary>
+        ///  callback method which updates application state for displaying icon 
+        /// </summary>
+        /// <param name="nCode"></param>
+        /// <param name="wParam"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        private static IntPtr callback(int nCode, IntPtr wParam, IntPtr lParam)
+        {
+            if (nCode >= 0 && wParam == (IntPtr)WM_KEYDOWN)
+            {
+                int vkCode = Marshal.ReadInt32(lParam);
+                if(Keys.CapsLock == (Keys)vkCode)
+                {
+                    icon.updateIcon();
+                }
+
+
+            }
+            return CallNextHookEx(hookId, nCode, wParam, lParam);
+        }
+
+        /// <summary>
+        /// set the Windows hook to handle the caps lock press event
+        /// </summary>
+        /// <param name="proc"></param>
+        /// <returns></returns>
+        private static IntPtr setKbHook(KeyboardDelegate kbDelegate)
+        {
+            using (Process curProcess = Process.GetCurrentProcess())
+            using (ProcessModule curModule = curProcess.MainModule)
+            {
+                return SetWindowsHookEx(WH_KEYBOARD_LL, kbDelegate, GetModuleHandle(curModule.ModuleName), 0);
+            }
+        }
+
+
+
+
+    }
+}
---
+++ CapsLock/CapsTrayIcon.cs
@@ -0,0 +1,102 @@
+﻿/*
+ The MIT License (MIT)
+
+Copyright (c) 2015 ericcdub
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE. 
+*/
+
+using CapsLock.Properties;
+using System;
+using System.Windows.Forms;
+
+namespace CapsLock 
+{
+
+    /// <summary>
+    /// This class encapsulates our app's system tray icon and the functionality involved in displaying and updating it
+    /// in response to caps lock events
+    /// 
+    /// The class implements IDisposable as it uses unmanaged resources (.ico images)
+    /// so we want to make sure they are cleaned up correctly
+    /// </summary>
+    class CapsTrayIcon : IDisposable
+    {
+        NotifyIcon icon;
+        bool state;
+        MenuItem exitMenuItem;
+        public void Dispose()
+        {
+            icon.Dispose();
+        }
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CapsTrayIcon"/> class.
+        /// </summary>
+
+        public CapsTrayIcon()
+        {
+            // Instantiate the NotifyIcon object.
+            icon = new NotifyIcon();
+            state = Control.IsKeyLocked(Keys.CapsLock);
+            exitMenuItem = new MenuItem();
+        
+        }
+
+        /// <summary>
+		/// Displays the icon in the system tray.
+		/// </summary>
+		public void Display()
+        {
+            // Add the icon to the system tray and allow it react to mouse right-clicks with a context menu		
+            icon.Icon = state ? Resources.upper : Resources.lower; // set tray icon based on current caps lock state
+            icon.Text = Resources.status + (state ? Resources.on : Resources.off); 
+            icon.Visible = true;
+
+            //create a context menu with an "exit" item
+            icon.ContextMenu = new System.Windows.Forms.ContextMenu();
+            exitMenuItem.Index = 0; // first (and only) menu item
+            exitMenuItem.Text = Resources.exit;
+            exitMenuItem.Click += new System.EventHandler(exitMenuItem_Click);
+            icon.ContextMenu.MenuItems.Add(exitMenuItem);
+        }
+
+        // update the icon and desription text of our icon in response to a caps lock state change
+        public void updateIcon()
+        {
+            state = !state;
+            icon.Icon = state ? Resources.upper : Resources.lower;
+            icon.Text = Resources.status + (state ? Resources.on : Resources.off);
+
+
+        }
+
+        /// <summary>
+        /// Handles the click of the "exit" menu item for our icon
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="System.Windows.Forms.EventArgs"/> instance containing the event data.</param>
+        void exitMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+           
+        }
+
+
+    }
+}
